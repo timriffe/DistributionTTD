bda9233c4b871d211396502cf1200f7a62a67a2f
triffe <triffe@demog.berkeley.edu>
2015-04-03 16:00:49 -0700

    minr edits to text. started to think of cohort calcs, which would make more sense anyway, but didn't get into it due to need for large code import...

--- DistributionTTD/R/DataPrep.R
+++ DistributionTTD/R/DataPrep.R
@@ -41,3 +41,37 @@ LT   <- data.table(LT)
 attr(LT,"timestamp") <- Sys.Date()
 # save out for repeated use
 save(LT, file = "Data/HMDltper.Rdata")
+
+
+############################################################################
+# make cohort lifetables: need to base it on cMx, import scripts as necessary
+
+#
+#LT <- do.call(rbind,lapply(Countries, function(XYZ, us, pw){
+#      # downloads just like yours, but fixes some column classes.
+#      cat(XYZ,"\n")
+#      LTM      <- readHMDweb(XYZ, "mltper_1x1", username = us, password = pw)   
+#      LTF      <- readHMDweb(XYZ, "fltper_1x1", username = us, password = pw) 
+#      LTM$Sex  <- "m"
+#      LTF$Sex  <- "f"
+#      LT       <- rbind(LTM, LTF)
+#      LT$CNTRY <- XYZ
+#      LT
+#    }, us = us, pw = pw))
+## order columns (no worries, Age is integer)
+#LT   <- LT[with(LT, order(CNTRY, Sex, Year, Age)),]
+## convert to data.table
+#
+#LT   <- data.table(LT)
+#
+## for 'Date Accessed' entry in HMD reference. 
+## Re-Run this script for the final version to regenerate clean results on latest data.
+#attr(LT,"timestamp") <- Sys.Date()
+## save out for repeated use
+#save(LT, file = "Data/HMDltper.Rdata")
+
+
+
+
+
+
---
+++ DistributionTTD/R/GeneralPatternsCohort.R
@@ -0,0 +1,16 @@
+
+
+# the surfaces produced in general patterns are are values derived from a series of independent
+# synthetic cohorts
+
+
+
+
+
+
+
+
+
+
+
+
