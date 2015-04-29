dd51c201461fd2b724864ed35bfa136f38b5d61e
Tim Riffe <tim.riffe@gmail.com>
2015-04-29 16:00:13 -0700

    playing on train with A

--- DistributionTTD/R/play.R
+++ DistributionTTD/R/play.R
@@ -109,3 +109,104 @@ LexisMap(SX, log= FALSE)
 mx <- acast(SWE, Age~Year, value.var = "mx")
 dev.new()
 LexisMap(mx)
+
+# ---------------------------------------------
+HMD <- local(get(load("/home/tim/git/DistributionTTD/DistributionTTD/Data/HMDltper.Rdata")))
+
+lx <- HMD$lx[HMD$CNTRY == "SWE" & HMD$Year == 2000 & HMD$Sex == "f"]/1e5
+
+myspline <- function(lx,y){
+	lxy <- lx/lx[1]
+	yy <- 1:length(lxy)-1
+	splinefun(lxy~yy,method="monoH.FC")(y)
+}
+
+maxA <- function(y,lx){
+	myspline(lx=lx,y=y)*y
+}
+
+#xA <- optimize(maxA,c(0,110),lx=lx,maximum=TRUE)$max
+
+getxA <- function(lx){
+	optimize(maxA,c(0,length(lx)-1),lx=lx,maximum=TRUE)$max
+}
+
+
+plot(0:110,lx,type='l')
+abline(v=getxA(lx))
+abline(h=myspline(lx,getxA(lx)))
+
+getA <- function(lx){
+	xA <- getxA(lx)
+	lxA <- myspline(lx,xA)
+	A <- xA * lxA
+	A / sum(lx)
+}
+getA(lx)
+
+lines(0:110, 100*c(-diff(c(-diff(c(lx,0)),0))))
+
+
+plot(0:110,lx*0:110,type='l',main="yup, looks like there's an age pattern")
+abline(v=getxA(lx))
+sapply(0:110,function(x,lx){
+			lxx <- lx[(x+1):length(lx)]
+			lines(x:(110),lxx* (0:(110-x)))
+		},lx=lx)
+
+
+lx
+plot(0:100,sapply(1:101,function(xx,lx){
+			lxx <- lx[(xx+1):length(lx)]
+			getxA(lxx)
+		},lx=lx),type='l',asp=1,ylab="xA(x)")
+abline(h=0)
+TEST <- sapply(1:101,function(xx,lx){
+			lxx <- lx[(xx+1):length(lx)]
+			getxA(lxx)
+		},lx=lx)
+getxAP <- function(lx){
+	if (any(lx==0)){
+		lx <- c(lx[lx>0],0)
+	}
+	xAy <- sapply(1:(length(lx)-2),function(xx,lx){
+				lxx <- lx[(xx+1):length(lx)]
+				getxA(lxx)
+			},lx=lx)
+	heights <- myspline(lx,xAy+(1:length(xAy)-1)) / lx[1:length(xAy)]
+	
+	c(xAy * heights,rep(0,111-length(xAy)))
+}
+
+
+library(reshape2)
+LX <- acast(HMD[HMD$CNTRY == "SWE" & HMD$Sex == "f", ], Age~Year, value.var = "lx")/1e5
+EX <- acast(HMD[HMD$CNTRY == "SWE" & HMD$Sex == "f", ], Age~Year, value.var = "ex")
+# stalls
+library(parallel)
+AXP <- do.call(cbind,mclapply(as.data.frame(LX),getxAP,mc.cores=4))
+dimnames(AXP) <- dimnames(LX)
+
+library(LexisUtils)
+prop <- AXP/EX
+prop[prop > 1] <- 1
+LexisMap(prop[1:95,],log=FALSE)
+head(AXY)
+range(AXY/EX)
+plot(LX[,"1900"])
+
+
+######################################################3
+# entropy?
+# min, when everyone dies at same age:
+# max when uniform?
+
+# edagger is null when all die at same time
+# max when death are uniform.
+dx <- -diff(c(lx,0))
+ex <-rev(cumsum(rev(lx))) / lx 
+plot(dx * ex)
+sum(dx*ex)
+
+
+
